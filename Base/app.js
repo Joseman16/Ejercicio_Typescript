b8df651e9dc8e4abe444178837640a4b20364812
Joseman16 <120001124+Joseman16@users.noreply.github.com>
2025-09-17 09:32:27 -0500

    Ejercicios de typescript

---
+++ Base/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//const msg = 'Hola mundo'
+const msg = 'Hola mundo cruel!';
+console.log(msg);
+//# sourceMappingURL=app.js.map
---
+++ Base/app.ts
@@ -0,0 +1,5 @@
+//const msg = 'Hola mundo'
+const msg:string = 'Hola mundo cruel!'
+console.log(msg)
+
+ 
