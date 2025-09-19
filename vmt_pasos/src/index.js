d4cdddfe563f69723e86c8795b6cf8bd940ba139
Josema <josemaleoncarrera@gmail.com>
2025-09-19 17:31:12 -0500

    Proyecto

---
+++ vmt_pasos/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const variable = "Josema";
+console.log("Hola mundo cruel, soy " + variable + "?");
+//# sourceMappingURL=index.js.map
---
+++ vmt_pasos/src/index.ts
@@ -0,0 +1,2 @@
+const variable = "Josema";
+console.log("Hola mundo cruel, soy " + variable+"?");
