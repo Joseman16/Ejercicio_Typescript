2ed84d0b342fb1e3e344bf820ae98c3b296e2860
Joseman16 <120001124+Joseman16@users.noreply.github.com>
2026-01-14 02:49:02 -0500

    Tipo de datos boolean, uso de AND (&&) y OR(||) aparte de crear un ternario para saber que va a devolver, todo eso lo guardamos en una función anonima autoinvocada, es donde va el codigo principal y asi nos evitamos de variables globales

--- Base/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const msg = 'Hola mundo!!!';
-const hero = {
-    name: 'Iroman',
-    age: 45
-};
-hero.age = 50;
-console.log(hero.age + 1);
-//# sourceMappingURL=main.js.map
---
+++ Bases/app.js
@@ -0,0 +1,35 @@
+(() => {
+    const a = 10;
+    console.log(a);
+})();
+
+/*
+message:string = "Hola mundo!";
+//console.log(message);
+const hero = {
+    name: 'Ironman',
+    age: 45
+}
+console.log(hero.age + 1)
+*/
+/*
+const palabra1: string = "Hola mundo";
+let palabra2: string = "Hola mundo cruel";
+let palabra3:string;
+palabra3 = "Hostia tio!!"
+console.log(palabra1);
+console.log(palabra2);
+console.log(palabra3);
+*/
+/*
+function sayHello(msg:string){
+    console.log("Hola "+msg);
+}
+
+function suma(num1: number, num2: number){
+    console.log("La suma: "+num1 + num2);
+}
+sayHello('Jose');
+suma(2,7);
+*/ 
+//# sourceMappingURL=app.js.map
