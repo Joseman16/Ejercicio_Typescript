c7e13449a634e412998c3c1b84beff4372a071f0
Josema <josemaleoncarrera@gmail.com>
2026-01-22 17:01:44 -0500

    Creación de arreglos, se puede colocar un arreglo de varios tipos si lo especificamos, pero no es lon coorecto, también creamos un arreglo foreach y un for tradicional para mostrar los datos de los arreglos de number y string

---
+++ Bases/tipos/arrays.ts
@@ -0,0 +1,22 @@
+(() =>{
+
+    const numbers = [1,2,3,4,5,6,7,8,9,10];
+    //const numbers:(string | number | boolean)[] = [1,2,3,4,5,'6',7,8,9,10]; (esto es raro, y no somos tan especificos)
+    //numbers.push(true);
+    
+    console.log(numbers);
+
+    const villians = ['Omega Rojo', 'Duende verde', 'Spiderman'];
+
+    console.log("\nVillanos en MAYUSCULAS:");
+    villians.forEach(v => console.log(v.toUpperCase()));
+
+    console.log("\nVillanos en minusculas:");
+    villians.forEach(v => console.log(v.toLowerCase()));
+
+    console.log("\nVillanos con for tradicional:");
+    for (let i = 0; i < villians.length; i++) {
+        console.log(villians[i]);
+    }
+
+}) ()
