c3b7137c4f6c4b6c77d2ffdb75dc2498df139795
Josema <josemaleoncarrera@gmail.com>
2026-02-02 16:50:35 -0500

    Objetos, Y la palabra reservada types para crear nuevos objetos con la estructura de aquel tipo

---
+++ Objetos/objetos02.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 22,
+        powers: ['Super Fuerza', 'Viajar en el tiempo']
+    };
+    let superman = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: [1]
+    };
+})();
+//# sourceMappingURL=objetos02.js.map
---
+++ Objetos/objetos02.ts
@@ -0,0 +1,16 @@
+(() =>{
+    let flash: {name: String, age?: number, powers:string[], getName?: () => string} = {
+        name: 'Barry Allen',
+        age:22,
+        powers: ['Super Fuerza', 'Viajar en el tiempo']
+    }
+
+    let superman:{name:String, age?:number, powers: number[], getName?: () => string} = {
+        name: 'Clark Kent',
+        age:60,
+        powers: [1]
+    }
+
+
+
+}) ()
