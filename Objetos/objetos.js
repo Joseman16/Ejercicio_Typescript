7c94f81eb58615d13b92a343a4d41fe504bd9568
Josema <josemaleoncarrera@gmail.com>
2026-02-02 05:41:55 -0500

    Creación de objetos y funciones (metodos), estos metodos estan dentro de los objetos, y podemos colocar el signo ? para decir que ese método sea opcional y poder utilizarlo a futuro

---
+++ Objetos/objetos.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+(() => {
+    let flash = {
+        name: 'Josman',
+        age: 22,
+        powers: ['Super Velocidad', 'Viajar en el tiempo']
+    };
+    // flash = {
+    //     name: 'Clark Kent',
+    //     age:60,
+    //     powers: ['Super Fuerza', 'Volar'],
+    //     getNombre (){
+    //         return this.name;
+    //     }
+    // }
+    let flash2 = {
+        name: 'Barry Allen',
+        age: 25,
+        powers: ['Super Velocidad', 'Viajar en el tiempo'],
+    };
+    //Esto sirve para agregar propiedades a un objeto ya declarado
+    flash2 = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super Fuerza', 'Volar'],
+        //Con el signo de interrogacion en la declaracion de la variable,
+        //indicamos que esa propiedad es opcional
+        getName() {
+            return this.name;
+        }
+    };
+    console.log(flash2.getName?.());
+})();
+//# sourceMappingURL=objetos.js.map
